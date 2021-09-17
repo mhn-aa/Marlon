777e71113b96ed3b2aed7658ab9cd5c02b8ba3e1
mhn-aa <mahanmirjamali@gmail.com>
2021-09-18 09:14:59 +1000

    did not observe the problem

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -16,6 +16,8 @@ const navLink = document.querySelectorAll(".nav__link");
 function linkAction() {
   navLink.forEach((n) => n.classList.remove("active"));
   this.classList.add("active");
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show");
 }
 
 navLink.forEach((n) => n.addEventListener("click, linkAction"));
